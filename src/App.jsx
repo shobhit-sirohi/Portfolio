a903bebe0ed89ec073050d41f00d70ffbb7468ed
shobhit <shobhitsirohi.dev@gmail.com>
2021-11-11 12:12:47 +0530

    update responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen px-24 pt-10 2xl:px-44 2xl:pt-32">
+    <div className="w-screen h-screen px-4 pt-4 lg:px-24 lg:pt-10 2xl:px-36 2xl:pt-10">
       <Navigation />
       <Home />
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,25 +4,25 @@ import ladka2 from "../images/ladka2.svg";
 
 const Home = () => {
   return (
-    <div className="flex w-full h-5/6">
-      <div className="flex flex-col justify-between w-1/2 mt-32 text-left 2xl:mt-48 font-DmSans ">
+    <div className="flex flex-col justify-between w-full md:flex-row md:h-5/6">
+      <div className="flex flex-col mt-10 text-left md:w-1/2 md:mt-32 2xl:mt-48 font-DmSans ">
         <div>
-          <h2 className="text-6xl font-medium 2xl:text-8xl">
+          <h2 className="text-4xl font-medium sm:text-5xl md:text-5xl lg:text-6xl 2xl:text-7xl">
             Namaste, I’m
           </h2>
-          <h2 className="mt-2 text-6xl font-medium 2xl:text-8xl">
+          <h2 className="mt-2 text-4xl font-medium sm:text-5xl md:text-5xl lg:text-6xl 2xl:text-7xl">
             Shobhit Sirohi.
           </h2>
-          <h3 className="mt-2 text-3xl font-medium 2xl:text-5xl font-DmSans text-textgrey">
-            I design & build digital products
+          <h3 className="mt-2 text-2xl font-medium md:text-3xl 2xl:text-4xl font-DmSans text-textgrey">
+            I design & build digital products.
           </h3>
         </div>
 
-        <div className="mousey">
+        {/* <div className="mousey">
           <div className="scroller"></div>
-        </div>
+        </div> */}
       </div>
-      <div className="flex items-center justify-center w-1/2 p-6">
+      <div className="items-center justify-center p-6 md:w-1/2 md:flex">
         <img
           className="h-full"
           src={ladka}
