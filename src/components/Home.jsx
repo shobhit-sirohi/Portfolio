c41da298f0b574180fb13342cb9b295e6594b773
shobhit <shobhitsirohi.dev@gmail.com>
2021-11-12 19:54:16 +0530

    mobile responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,17 +8,17 @@ const Home = () => {
     <div className="flex flex-col w-full md:flex-row md:h-5/6">
       <div className="flex flex-col justify-between mt-10 text-left md:w-1/2 md:mt-32 2xl:mt-48 font-DmSans ">
         <div>
-          <h2 className="text-4xl font-medium sm:text-5xl md:text-5xl lg:text-6xl 2xl:text-7xl">
+          <h2 className="text-5xl font-medium lg:text-6xl 2xl:text-7xl">
             Namaste, I’m
           </h2>
-          <h2 className="mt-2 text-4xl font-medium sm:text-5xl md:text-5xl lg:text-6xl 2xl:text-7xl">
+          <h2 className="mt-2 text-5xl font-medium sm:text-5xl md:text-5xl lg:text-6xl 2xl:text-7xl">
             Shobhit Sirohi.
           </h2>
-          <h3 className="mt-2 text-2xl font-medium md:text-3xl 2xl:text-4xl font-DmSans text-textgrey">
+          <h3 className="mt-2 text-3xl font-medium md:text-3xl 2xl:text-4xl font-DmSans text-textgrey">
             I design & build digital products.
           </h3>
         </div>
-        <div>
+        <div className="hidden lg:flex">
           <img
             className="w-8 h-8"
             src={arrow}
