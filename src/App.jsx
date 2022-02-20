8547a704039737f72154663851f527b902020567
Shobhit Sirohi <shobhitsirohi.dev@gmail.com>
2022-02-20 23:38:46 +0530

    update logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ReactGA from "react-ga";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import WorkPage from "./components/WorkPage";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+const ID = "G-YMMDXFPM5H";
+ReactGA.initialize(ID);
 
 const App = () => {
   return (
