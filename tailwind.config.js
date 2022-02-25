e67f3b1a5e8bf87a0bbd5c7978c82afbcdd9040c
Shobhit Sirohi <shobhitsirohi.dev@gmail.com>
2022-02-26 03:01:28 +0530

    cursor update

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         blu: "#2429AF",
       },
       cursor: {
-        'fancy': 'url(./src/images/pointer.svg)',
+        'fancy': 'url(./src/images/pointer.svg),pointer',
       },
       fontFamily: {
         DmSans: ["DM Sans", "sans-serif"],
